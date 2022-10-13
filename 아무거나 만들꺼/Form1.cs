8ec1e0d111cf13c7b20f40f8f8aa3fd91e8621ba
zkcl1012 <dlalsrl1011@naver.com>
2022-10-13 22:45:34 +0900

    sign up

---
+++ 아무거나 만들꺼/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 아무거나_만들꺼
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            if (textBox1.Text == "minki05622" && textBox2.Text == "dlalsrl2344@")
+            {
+                MessageBox.Show("로그인 성공","로그인", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                
+            }
+            else 
+            {
+                MessageBox.Show("로그인 실패", "꺼져", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+          
+            
+
+        }
+    }
+}
---
+++ 아무거나 만들꺼/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace 아무거나_만들꺼
+{
+    public partial class Form2 : Form
+    {
+        string Conn = "Server=localhost;Database=gege;Uid=root;Pwd=dlalsrl2344@";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(Conn))
+            {
+                conn.Open();
+                MySqlCommand msc = new MySqlCommand("insert into ui(email, ID, PW) values('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "') ", conn);
+                int a = msc.ExecuteNonQuery();
+                if (a == 1)
+                {
+                    MessageBox.Show("정상적으로 입력되었습니다");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Close();
+
+        }
+    }
+}
